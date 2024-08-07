09b31089cbf473e85da460184a1de7a5420c31d8
rfantonial15 <fantonial.randulf9@gmail.com>
2024-08-07 13:37:50 +0800

    login fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import logo from '../assets/AIDA-Logo.png';
 import background from '../assets/Landing-Image.png';
-import '@fontsource/inter'; // Import Inter font
 
 const API_LOGIN_ENDPOINT = 'http://127.0.0.1:8000/api/login/';
 
@@ -16,27 +15,33 @@ function Login() {
   const handleLogin = async (event) => {
     event.preventDefault();
 
+    // try {
+    //   const response = await axios.post(API_LOGIN_ENDPOINT, { email, password });
+    //   console.log('Login successful:', response.data);
+    //   localStorage.setItem('token', response.data.token);
+    //   navigate('/home');
+    // } catch (error) {
+    //   console.error('Login failed:', error.response ? error.response.data : error.message);
+    //   setError('Invalid email or password');
+    // }
+
     try {
-      const response = await axios.post(API_LOGIN_ENDPOINT, { email, password });
-      console.log('Login successful:', response.data);
-      localStorage.setItem('token', response.data.token);
-      navigate('/home');
+      navigate('/dashboard');
     } catch (error) {
-      console.error('Login failed:', error.response ? error.response.data : error.message);
-      setError('Invalid email or password');
+
     }
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="flex w-full max-w-4xl h-auto bg-white rounded-lg shadow-lg overflow-hidden">
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex w-full max-w-1xl h-4/5 pl-14 pr-14 bg-white overflow-hidden">
         <div
-          className="hidden md:flex flex-1 bg-cover bg-center"
+          className="flex-1 bg-cover bg-center rounded-lg"
           style={{ backgroundImage: `url(${background})` }}
         ></div>
-        <div className="flex-1 p-8 md:p-12 flex flex-col justify-center items-center">
+        <div className="flex-1 pl-20 flex flex-col justify-center items-center">
           <img src={logo} alt="Logo" className="w-24 mb-8" />
-          <form className="w-full" onSubmit={handleLogin}>
+          <form className="w-full pl-36 pr-36" onSubmit={handleLogin}>
             {error && <div className="mb-4 text-red-500">{error}</div>}
             <div className="mb-4 w-full">
               <input
@@ -45,7 +50,7 @@ function Login() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
-                className="w-full p-3 border border-gray-300 rounded"
+                className="w-full p-3 border border-gray-500 rounded"
               />
             </div>
             <div className="mb-4 w-full">
@@ -55,7 +60,7 @@ function Login() {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
-                className="w-full p-3 border border-gray-300 rounded"
+                className="w-full p-3 border border-gray-500 rounded"
               />
             </div>
             <button
